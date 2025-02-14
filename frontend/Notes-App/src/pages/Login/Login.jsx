eb3f234fb91b4ae43f8dac68bd7ec4af3fd4456a
Prasath <prasathkanishk407@gmail.com>
2025-02-14 12:51:53 +0530

    Navigation Bar Completed

--- frontend/Notes-App/src/pages/Login/Login.jsx
+++ frontend/Notes-App/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import { Link } from 'react-router-dom'
-import PasswordInput from '../../components/Navbar/Input/PasswordInput'
+import PasswordInput from '../../components/Input/PasswordInput'
 import { validateEmail } from '../../utils/helper'
 
 const Login = () => {
