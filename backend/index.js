8f926476eec1fa8d20c8786da42784454acafb22
Prasath <prasathkanishk407@gmail.com>
2025-02-20 23:19:44 +0530

    Login API

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,7 @@ const app = express();
 
 const jwt = require("jsonwebtoken");
 const {authenticateToken} = require("./utilities");
+const userModel = require("./models/user.model");
 
 app.use(express.json());
 
@@ -69,6 +70,43 @@ app.post("/create-account", async (req,res) =>{
         message:"User Registration Successful",
     });
 
+});
+
+app.post("/login", async(req,res) =>{
+    const {email,password} = req.body;
+
+    if(!email){
+        return res.status(400).json({error:true, message:"Email is required"});
+    }
+
+    if(!password){
+        return res.status(400).json({error:true, message:"Password is required"});
+    }
+
+    const userInfo = await User.findOne({email:email});
+
+    if(!userInfo){
+        return res.status(400).json({message:"User not found"});
+    }
+
+    if(userInfo.email == email && userInfo.password == password){
+        const user = {user:userInfo};
+        const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:"3600m",
+        });
+
+        return res.json({
+            error: false,
+            message: "Login Successful",
+            email,
+            accessToken,
+        });
+    } else {
+        return res.status(400).json({
+            error:true,
+            message:"Invalid Credentials",
+        });
+    }
 });
 
 app.listen(8000);
