7e86296eb1e2e405cea1998926fc05c451feef04
Prasath <prasathkanishk407@gmail.com>
2025-02-25 18:59:11 +0530

    Login, Signup and Getuser API Integrated

--- frontend/Notes-App/src/pages/SignUp/SignUp.jsx
+++ frontend/Notes-App/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import PasswordInput from '../../components/Input/PasswordInput';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance'
 
 const SignUp = () => {
 
@@ -10,6 +11,8 @@ const SignUp = () => {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleSignUp = async(e) => {
     e.preventDefault();
@@ -30,6 +33,32 @@ const SignUp = () => {
     }
 
     setError("")
+
+    try{
+      const response = await axiosInstance.post("/create-account",{
+        fullName: name,
+        email: email,
+        password: password,
+      });
+
+      if(response.data && response.data.error){
+        setError(response.data.message)
+        return
+      }
+
+      if(response.data && response.data.accessToken){
+        localStorage.setItem('token',response.data.accessToken);
+        navigate('/dashboard');
+      }
+
+    }catch(error){
+      if (error.response && error.response.data && error.response.data.message){
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error occurred. Please try again later.");
+      }
+    }
+
   }
   return (
     <>
